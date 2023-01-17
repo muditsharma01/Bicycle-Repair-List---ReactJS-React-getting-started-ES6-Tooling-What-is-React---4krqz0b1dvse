1eea2ad15836ef77de2595bf0c5d181fa50ba497
muditsharma01 <muditshar001@gmail.com>
2023-01-17 21:47:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
+import Form from "./Form";
+import List from "./List";
 
-const App = () => {
+function App() {
   return (
-    <div id="main"></div>
-  )
+    <div className="App">
+      <header>Bicycle Repair App</header>
+      <Form />
+      <hr></hr>
+      <List />
+    </div>
+  );
 }
-
 
 export default App;
