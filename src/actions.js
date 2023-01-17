15a95c6efbc940a873f0ec4e82a38acae2716718
muditsharma01 <muditshar001@gmail.com>
2023-01-17 21:46:53 +0530

    Initial commit

---
+++ src/actions.js
@@ -0,0 +1,15 @@
+/*
+Here all the actions are defined.
+Example of defining an actoin is as follows
+
+export const repairAdded = (var1,va2) => {
+  return {
+    type: "actionType1",
+    payload: {
+      var1,
+      var2
+    }
+  }
+}
+
+*/
---
+++ src/reducer.js
@@ -0,0 +1,21 @@
+/*
+reducer is defined here and is exproted.
+This reducer should handle all the actions.
+Example of how to use reducer is as follows:
+
+const reducer = (state = [],action = {}) => {
+
+  switch(action.type){
+
+    case 'actionType1': 
+      return changedState1;
+    
+    case 'actionType2':
+      return changedState2;
+
+    default: 
+      return state;
+    }
+  
+export default reducer;
+*/
