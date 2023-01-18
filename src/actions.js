a5f27fb348d3976a5550f67fb30a6a4f7f6df254
muditsharma01 <muditshar001@gmail.com>
2023-01-18 21:25:38 +0530

    Update actions.js

--- src/actions.js
+++ src/actions.js
@@ -13,3 +13,52 @@ export const repairAdded = (var1,va2) => {
 }
 
 */
+export const repairAdded = ({ owner, model, description }) => {
+  return {
+    type: "repairAdded",
+    payload: {
+      owner,
+      model,
+      description,
+    },
+  };
+};
+
+export const repairRemoved = (id) => {
+  return {
+    type: "repairRemoved",
+    payload: {
+      id,
+    },
+  };
+};
+export const repairResolved = (id) => {
+  return {
+    type: "repairResolved",
+    payload: {
+      id,
+    },
+  };
+};
+export const repairUpdate = ({ id, owner, model, description }) => {
+  return {
+    type: "repairUpdate",
+    payload: {
+      id,
+      owner,
+      model,
+      description,
+    },
+  };
+};
+export const editTask = ({ id, owner, model, description }) => {
+  return {
+    type: "editTask",
+    payload: {
+      id,
+      owner,
+      model,
+      description,
+    },
+  };
+};
